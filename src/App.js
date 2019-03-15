243a1a862003b69630dbd6c8087eb291178546a8
Janusz Kobylarz <j.kobylarz@live.com>
2019-03-15 23:37:12 +0100

    Get Data from API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,152 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 
-class SearchForm extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {value: ""}
+class Person {
+    id;
+    name;
+    height;
+    mass;
+    hairColor;
+    skinColor;
+    eyeColor;
+    birthYear;
+    gender;
 
-        this.handleChange =  this.handleChange.bind(this);
-        this.handleSubmit =  this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
-
-    render(){
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        )
+    constructor(result) {
+        this.id = result.id;
+        this.name = result.name;
+        this.height = result.height;
+        this.mass = result.mass;
+        this.hairColor = result.hairColor;
+        this.skinColor = result.skinColor;
+        this.eyeColor = result.eyeColor;
+        this.birthYear = result.birthYear;
+        this.gender = result.gender;
     }
 }
 
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.searchPhrase = {
+            value: null
+        }
+        this.state = {
+            searchPhrase: "",
+            error: null,
+            isLoaded: false,
+            results: false,
+            Person: null
+        };
+        this.apiurl = "http://vps633295.ovh.net/starwars/api/name?name=";
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleChange(event) {
+        this.setState({searchPhrase: event.target.value});
+    }
+
+    handleSubmit(event) {
+        fetch(this.apiurl + this.state.searchPhrase, {
+            method: "GET"
+        }).then(res => res.json())
+            .then(
+                (result) => {
+                    if (!result) {
+                        this.setState({
+                            isLoaded: true,
+                            results: false,
+                            error: "Result not found"
+                        });
+                    } else {
+                        this.setState({
+                            isLoaded: true,
+                            results: true,
+                            Person: new Person(result),
+                            error: null
+                        });
+                    }
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        results: false,
+                        error: "Generic error"
+                    });
+                }
+            )
+
+        event.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-6 mx-auto">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name:
+                        </label>
+                        <input className="form-control" type="text" value={this.state.searchPhrase}
+                               onChange={this.handleChange}/>
+                        <input className="btn btn-primary" type="submit" value="Submit"/>
+                        <div className="error">{this.state.error}</div>
+                        <Results data={this.state}/>
+                    </form>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+function Results(state) {
+    if (state.data.results) {
+        let person = state.data.Person;
+        return (<table className="table table-striped">
+                <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>{person.name}</td>
+                </tr>
+                <tr>
+                    <td>Height</td>
+                    <td>{person.height}</td>
+                </tr>
+                <tr>
+                    <td>Mass</td>
+                    <td>person.mass}</td>
+                </tr>
+                <tr>
+                    <td>Hair color</td>
+                    <td>{person.hairColor}</td>
+                </tr>
+                <tr>
+                    <td>Skin color</td>
+                    <td>{person.skinColor}</td>
+                </tr>
+                <tr>
+                    <td>Eye color</td>
+                    <td>{person.eyeColor}</td>
+                </tr>
+                <tr>
+                    <td>Birth year</td>
+                    <td>{person.birthYear}</td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td>{person.gender}</td>
+                </tr>
+                </tbody>
+            </table>
+        );
+    }
+    return null;
+
+}
 
 export default SearchForm;
